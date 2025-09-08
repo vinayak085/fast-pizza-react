3fbf6f4a77cffdf0a1c01c10c58a11af917c615c
vinayak <vt869309@gmail.com>
2025-09-08 11:54:32 +0530

    before tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./features/ui/Home"
+import Error from "./features/ui/Error"
+import Menu,{loader as menuLoader} from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder,{action as createOrderAction} from "./features/order/CreateOrder"
+import Order,{loader as orderLoader} from "./features/order/Order"
+import AppLayout from "./features/ui/AppLayout"
+
+
+const router = createBrowserRouter([
+
+  {
+    element:<AppLayout/>,
+    errorElement:<Error/>,
+    children:[
+       {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/menu",
+    element:<Menu/>,
+    errorElement:<Error/>,
+    loader: menuLoader,
+  },
+  {
+    path:"/cart",
+    element:<Cart/>
+  },
+  {
+    path:"/order/new",
+    element:<CreateOrder/>,
+    action:createOrderAction
+  },
+  {
+    path:"/order/:orderId",
+    element:<Order/>,
+    loader:orderLoader,
+    errorElement:<Error/>,
+  }
+    ]
+  }
+
+])
+
+function App() {
+
+  return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/features/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import SearchOrder from '../order/SearchOrder'
+
+function Header() {
+  return (
+    <header>
+     <Link to="/">Fast React Pizza Co.</Link>
+     <SearchOrder/>
+      
+     <p>Jonas </p>
+    </header>
+  )
+}
+
+export default Header
